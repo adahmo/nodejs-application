195a4dc535e0a35c5b296c2f731d69a914a853da
adahmo <118616463+adahmo@users.noreply.github.com>
2023-01-07 23:50:57 -0400

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
-	    'sonar.login': '962aaaef80f6b616d5c9471aeb2046a314b81178',
+	    'sonar.login': 'eb48e0192d7cf581dd763506cfdd593f8502c7f1',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
